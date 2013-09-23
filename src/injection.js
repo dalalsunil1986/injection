6317f0d5aa5287eb3ce23496b7b5d1ab2219d16f
Andrew Mead <andrewjosephmead@gmail.com>
2013-09-22 20:17:33 -0400

    updated test cases

--- src/injection.js
+++ src/injection.js
@@ -7,56 +7,59 @@
  * @param data {object} initial dependencies
  */
 function Inject(data) {
-  this.dependencies = data;
+  this.dependencies = data || {};
 }
-
 
 /**
  * @method execute
  * process a function and inject its dependencies
  */
 Inject.prototype.execute = function (func) {
-  func.apply(undefined,
-      this._getDependencies(func)
-  );
-};
+  var that = this,
+      deps = this._getDependencies(func);
 
+  deps = deps.map(function (arg) {
+    return that.dependencies[arg];
+  });
+
+  func.apply(undefined, deps);
+};
 
 /**
  * @method _getDependencies
  * internal method for parsing a functions for dependencies
  */
 Inject.prototype._getDependencies = function (func) {
-  var that = this,
-      STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg,
+  var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg,
       fnStr = func.toString().replace(STRIP_COMMENTS, ''),
       result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(/([^\s,]+)/g) || [];
 
-  return result.map(function (arg) {
-    return that.dependencies[arg];
-  });
+  return result;
 };
-
 
 /**
  * @method register
  * register a new dependency
+ *
+ * @param key {string}
+ * @param value {string}
  */
-Inject.prototype.register = function (key, val) {
-  return this.dependencies[key] = val;
+Inject.prototype.register = function (key, value) {
+  this.dependencies[key] = value;
+  return this;
 };
 
 /**
  * @example
  *
  *
-var apis = new Inject({
+ var apis = new Inject({
   url: '/api/v1'
 });
 
-apis.register('saying', 'yo ho, yo ho!');
+ apis.register('saying', 'yo ho, yo ho!');
 
-apis.execute(function (saying, test, url) {
+ apis.execute(function (saying, test, url) {
   console.log(saying); // > yo ho, yo ho!
   console.log(test); // > undefined
   console.log(url); // > /api/v1
--- test/injection.test.js
+++ test/injection.test.js
@@ -2,4 +2,73 @@
  * test suite for injeciton
  */
 
-var mocha = require('mocha');
+var Inject = require('../src/injection'),
+    should = require('should');
+
+describe('Injection', function () {
+
+  /* tests for the constructor function */
+  describe('#Injection', function () {
+
+    it('should attach data to dependencies', function () {
+      var obj = {"name": "andrew"},
+          inject = new Inject(obj);
+
+      inject.dependencies.should.eql(obj);
+    });
+
+    it('should default this.dependencies to obj if no defaults are provided', function () {
+      var inject = new Inject();
+      inject.dependencies.should.eql({});
+    });
+
+  });
+
+  /* gets for getting functions deps */
+  describe('#_getDependencies', function () {
+    function func(one, two, three, a, b, c) {
+    }
+
+    it('should return an array of dependencies', function () {
+      var result = Inject.prototype._getDependencies.call({dependencies: {}}, func);
+
+      result.should.eql(['one', 'two', 'three', 'a', 'b', 'c'])
+    });
+  });
+
+  describe('#register', function () {
+
+    it('should add the key value pair to the dependencies', function () {
+      var result = Inject.prototype.register.call({dependencies: {}}, '1', 'one');
+
+      result.dependencies['1'].should.equal('one');
+    });
+
+  });
+
+  it('should correctly inject dependencies', function () {
+    var inject = new Inject({
+      "name": "andrew"
+    });
+
+    function sayHi(name) {
+      name.should.equal('andrew');
+    }
+
+    inject.execute(sayHi);
+  });
+
+  it('should resolve undefined dependencies as undefined', function () {
+    var inject = new Inject({
+      "lastName": "name"
+    });
+
+    function sayHi(firstName, lastName) {
+      should.not.exist(firstName);
+      lastName.should.equal('name');
+    }
+
+    inject.execute(sayHi);
+  });
+
+});
